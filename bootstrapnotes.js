71fc33c9e31723e2a25c4ef4e5139080bbd12289
Jessica Hathaway <hathaway.jess@gmail.com>
2016-05-23 23:07:05 -0600

    added notes

---
+++ bootstrapnotes.js
@@ -0,0 +1,34 @@
+***BOOTSTRAP***
+
+What is Bootstrap?
+	-a free and open source front-end framework
+	-advantages: responsive by design, consistent, wide compatibility, easy to learn
+	-contains HTML/CSS design templates and optional JavaScript extensions
+	-created by developers at Twitter (originally called Twitter Blueprint)
+
+How do I get Bootstrap? http://getbootstrap.com/
+	-link to CDN
+	-download from website and extract into project folder
+	-bower install
+	-npm install
+
+Alternatives: http://designsparkle.com/bootstrap-alternatives/
+	- Materialize : http://materializecss.com/
+	- Foundation : http://foundation.zurb.com/
+	- HTML5 Boilerplate : https://html5boilerplate.com/
+	- Pure : http://purecss.io/
+
+Lightweight Alternatives: http://www.hongkiat.com/blog/bootstrap-alternatives/
+		- Skeleton : http://getskeleton.com/
+		- CardinalCSS : http://cardinalcss.com/
+
+
+Who uses Bootstrap? http://expo.getbootstrap.com/
+	- Twitter uses components of Bootstrap, but it is not built entirely with Bootstrap
+	- http://calhoun.ca/
+	- https://www.mongodb.com/cloud
+
+--------------------------------------------------------------------------------------------
+
+
+http://www.bootply.com/63838
